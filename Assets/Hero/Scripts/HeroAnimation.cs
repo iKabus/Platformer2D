7358ab98e29ab9a8e378f1fab4232821320ebf15
Nikolay Semenov <164047031+iKabus@users.noreply.github.com>
2025-01-08 11:35:05 +0300

    Reserve

---
+++ Assets/Hero/Scripts/HeroAnimation.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Mover))]
+[RequireComponent(typeof(Animator))]
+
+public class HeroAnimation : MonoBehaviour
+{
+    private Mover _mover;
+    private Animator _animator;
+    
+    private int _isRunningHash = Animator.StringToHash("isRunning");
+    private int _isStopHash = Animator.StringToHash("isStop");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _mover = GetComponent<Mover>();
+    }
+
+    private void OnEnable()
+    {
+        _mover.Moving += Run;
+        _mover.Stoping += Stop;
+        _mover.Reflecting += Reflect;
+    }
+
+    private void OnDisable()
+    {
+        _mover.Moving -= Run;
+        _mover.Stoping -= Stop;
+        _mover.Reflecting -= Reflect;
+    }
+
+    private void Run()
+    {
+        _animator.SetTrigger(_isRunningHash);
+    }
+
+    private void Stop()
+    {
+        _animator.SetTrigger(_isStopHash);
+    }
+
+    private void Reflect()
+    {
+        int direction = -1;
+        
+        transform.localScale *= new Vector2(direction,1);
+    }
+}
---
+++ Assets/Hero/Scripts/Mover.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public const String CommandHorizontal = "Horizontal";
+    
+    [SerializeField, Min(0.1f)] private float _speed;
+    [SerializeField, Min(5f)] private float _jumpForce;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _ground;
+    
+    public event Action Moving;
+    public event Action Stoping;
+    public event Action Reflecting;
+    
+    private Coroutine _coroutine;
+    
+    private Rigidbody2D _rigidBody2D;
+
+    private float _minSpeed = 0.1f;
+    private float _groundCheckRadius = 0.3f;
+
+    private Vector2 _inputVector;
+    
+    private bool _isRight = true;
+    private bool _onGround = true;
+
+    private void Awake()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _coroutine = StartCoroutine(CheckingGround());
+    }
+    
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+    
+    private void OnDestroy()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }    
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _onGround)
+        {
+            _rigidBody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void Move()
+    {
+        _inputVector.x = Input.GetAxis(CommandHorizontal);
+        
+        _rigidBody2D.linearVelocity = new Vector2(_inputVector.x * _speed, _rigidBody2D.linearVelocity.y);
+        
+        if (Mathf.Abs(_inputVector.x) > _minSpeed)
+        {
+            Moving?.Invoke();
+        }
+        else
+        {
+            Stoping?.Invoke();
+        }
+        
+        if((_inputVector.x > 0 && _isRight == false) || (_inputVector.x < 0 && _isRight))
+        {
+            Reflecting?.Invoke();
+            
+            _isRight = !_isRight;
+        }
+    }
+
+    private IEnumerator CheckingGround()
+    {
+        var wait = new WaitForSeconds(0.1f);
+
+        while (enabled)
+        {
+            CheckGround();
+            
+            yield return wait;
+        }
+    }
+
+    private void CheckGround()
+    {
+        _onGround = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _ground);
+    }
+}
